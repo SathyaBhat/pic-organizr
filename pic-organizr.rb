0d2dc54f98af6e4722991b5c916f83f6c7c621b6
SathyaBhat <sathya@sathyasays.com>
2013-09-29 18:18:37 +0530

    typo in Fileutils

--- pic-organizr.rb
+++ pic-organizr.rb
@@ -6,5 +6,5 @@ Dir.chdir(path)
 Dir.glob('*.jpg').each { |file|
   exif_info = EXIFR::JPEG.new(file)
   Dir.mkdir(exif_info.make || "No Make") if not Dir.exist?(exif_info.make || "No Make")
-  Fileutils.move file, exif_info.make || "No Make"
+  FileUtils.move file, exif_info.make || "No Make"
   }
